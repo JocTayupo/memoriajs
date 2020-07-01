6574ccd08e59c7148ec0d191089ff8742a693420
Jose Tayupo <josetayupo@gmail.com>
2020-06-30 20:46:03 -0400

    Generando colores semi aleatoriamente

--- js/main.js
+++ js/main.js
@@ -1,3 +1,61 @@
 function main(){
-    
+    let tarjetas = document.getElementsByClassName('tarjeta');
+    let colores = ['#00BD9D','#CD5334','#2E282A','#113537','#F7B32B','#667761','#87D68D','#FF715B','#5D5F71'];
+    let indicesTarjetas = [];
+    let indice,seleccion,tarjeta1,tarjeta2,indicet1,indicet2,elemento;
+    ///Preparar indice de las tarjetas
+    for (let i = 0; i < tarjetas.length; i++) {
+        indicesTarjetas.push(i);       
+    }
+    console.log(indicesTarjetas);
+    //sortear figuras
+    for (i = 0; i < 6; i++) {
+        indice = parseInt(Math.random() * (colores.length - 0) + 0);
+        seleccion = colores.splice(indice,1);
+        elemento = document.createElement("div"); 
+        elemento.classList.add('circulo');
+        elemento.style.backgroundColor = seleccion ;
+
+        elemento2 = document.createElement("div"); 
+        elemento2.classList.add('circulo');
+        elemento2.style.backgroundColor= seleccion;
+        //seleccionar tarjeta 1
+        indicet1 = parseInt(Math.random() * (indicesTarjetas.length - 0) + 0);
+        indicet2 = parseInt(Math.random() * (indicesTarjetas.length - 0) + 0);
+        indicet1 = indicesTarjetas.indexOf(indicet1);
+        indicet2 = indicesTarjetas.indexOf(indicet2);
+
+        tarjeta1 = indicesTarjetas.splice(indicet1,1);
+        tarjeta2 = indicesTarjetas.splice(indicet2,1);
+        tarjetas[tarjeta1].appendChild(elemento);
+        tarjetas[tarjeta2].appendChild(elemento2);
+
+        console.log(`${tarjeta1} - ${tarjeta2}`);
+        console.log(indicesTarjetas);
+
+    } 
+/*    let numeros = [3,9,5,1,7,2,8,4,6];
+   const parejas = 6;
+
+   class Tarjeta{
+        constructor(numero){
+            this.estados = [1,0];
+            this.numero = numero;
+            this.estado = 1; 
+        }
+
+        iniciarTarjetas(){
+            let divs = document.getElementsByClassName('tarjeta';)
+        }
+
+    } */
+
 }
+
+
+/*
+  1 - Distribuir la imagen en las 12 tarjetas :
+      * seleccionar los seis numeros 
+      * actualizar las tarjetas 
+
+*/
